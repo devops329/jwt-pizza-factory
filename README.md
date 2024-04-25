57a7f49bacb9f27ed39f934d4f95b2976bdbf6ea
Lee <lee.jensen@agilix.com>
2024-04-25 08:45:05 -0600

    enhance(endpoints) to match actual usage

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 Factory service for making NFT pizzas
 
+## Usage
+
+- Admin endpoint to register NFT Pizza implementation this returns an API key for the
+
+- Create JWT given an order
+
+  ```sh
+   curl -X POST localhost:3000/order -H 'authorization: abce4567-dddd-eeee-a345-996141749213' -d '{"store": "storeId-zzz", "order": {"pizzas":["pep", "cheese"]}}' -H 'Content-Type: application/json'
+  ```
+
+- Verify and order JWT
+  ```sh
+   curl -X POST localhost:3000/order/verify -d '{"order":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmFuY2hpc2UiOnsiaWQiOiIxMjM0NDU2Ny1mZmZmLXp6enotYTM0NS05OTYxNDE5MmFiY2QiLCJuYW1lIjoiU3VwZXJQaWUifSwic3RvcmUiOiJzdG9yZUlkLXp6eiIsIm9yZGVyIjp7InBpenphcyI6WyJwZXAiLCJjaGVlc2UiXX0sImlhdCI6MTcxNDA1NTcyOSwiZXhwIjoxNzE0MTQyMTI5LCJpc3MiOiJjczMyOS5jbGljayJ9.xcHR31MMMm3b4NR2xIHV4obryFvg0ytYDYkUmGF9wJ4"}' -H 'Content-Type: application/json'
+  ```
+
 ## Development notes
 
 1. Initialize node.js
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-// curl -X POST localhost:3000/order -H 'authorization: abce4567-dddd-eeee-a345-996141749213' -d '{"franchise":"monkeypie", "store": "provo", "order": {"pizzas":["pep", "cheese"]}}' -H 'Content-Type: application/json'
-// curl -X POST localhost:3000/order/verify -H 'authorization: abce4567-dddd-eeee-a345-996141749213' -d '{"jwt":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmFuY2hpc2UiOiJtb25rZXlwaWUiLCJzdG9yZSI6InByb3ZvIiwib3JkZXIiOnsicGl6emFzIjpbInBlcCIsImNoZWVzZSJdfSwiaWF0IjoxNzE0MDAwMTY4LCJleHAiOjE3NDU1MzYxNjgsImlzcyI6ImNzMzI5LmNsaWNrIn0.KJGn675PXpKppdwbEuOND-7mB24BGJMos0-Wd-PXLWU"}' -H 'Content-Type: application/json'
-
 const app = express();
+const jwtSecret = 'supersecret99';
 const apiKeys = {
-  'abce4567-dddd-eeee-a345-996141749213': '123e4567-e89b-12d3-a456-426614174000',
+  'abce4567-dddd-eeee-a345-996141749213': { id: '12344567-ffff-zzzz-a345-99614192abcd', name: 'SuperPie' },
 };
 
 app.use(express.json());
@@ -14,35 +12,36 @@ app.use(express.json());
 const verifyApiKey = (req, res, next) => {
   const apiKey = req.headers.authorization;
   if (apiKey && apiKeys[apiKey]) {
-    req.secretKey = apiKeys[apiKey];
+    req.franchiseInfo = apiKeys[apiKey];
     next();
   } else {
-    return res.status(401).json({ message: 'No API key provided' });
+    return res.status(401).json({ message: 'Invalid API key' });
   }
 };
 
 app.post('/order', verifyApiKey, (req, res) => {
-  const { franchise, store, order } = req.body;
-  if (!franchise || !store || !order) {
+  const { store, order } = req.body;
+  if (!store || !order) {
     return res.status(400).json({ message: 'Missing required parameters' });
   }
 
-  const token = jwt.sign({ franchise, store, order }, req.secretKey, {
-    expiresIn: '365d',
+  const token = jwt.sign({ franchise: req.franchiseInfo, store, order }, jwtSecret, {
+    expiresIn: '1d',
     issuer: 'cs329.click',
   });
 
   res.json({ token });
 });
 
-app.post('/order/verify', verifyApiKey, (req, res) => {
-  const encodedJwt = req.body.jwt;
+app.post('/order/verify', (req, res) => {
+  const orderJwt = req.body.order;
 
-  jwt.verify(encodedJwt, req.secretKey, (err, decodedJwt) => {
-    if (err) {
-      return res.status(403).json({ message: 'Failed to authenticate jwt' });
-    }
-    res.json({ order: decodedJwt });
+  const d = jwt.decode(orderJwt);
+  jwt.verify(orderJwt, jwtSecret, (err, decodedJwt) => {
+    const status = err ? 403 : 200;
+    const message = err ? 'invalid order' : 'valid order';
+    const result = { message: message, order: d };
+    res.status(status).json(result);
   });
 });
 
