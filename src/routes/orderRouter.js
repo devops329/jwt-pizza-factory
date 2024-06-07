0d61fa8e2e9342a6e59fe3f77217fd82e8098dbe
Lee <lee.jensen@agilix.com>
2024-06-07 08:51:06 -0600

    enhance(docs) with response examples

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -12,6 +12,9 @@ orderRouter.endpoints = [
     requiresAuth: true,
     description: 'Create a JWT pizza',
     example: `curl -X POST localhost:3000/api/order -H 'authorization: Bearer a42nkl3fdsfagfdagnvcaklfdsafdsa9' -d '{"diner": {"name":"joe"}, "order": {"pizzas":["pep", "cheese"]}}' -H 'Content-Type: application/json'`,
+    response: {
+      jwt: 'JWT here',
+    },
   },
   {
     method: 'POST',
@@ -19,6 +22,14 @@ orderRouter.endpoints = [
     requiresAuth: true,
     description: 'Verifies a pizza order',
     example: `curl -X POST localhost:3000/api/order/verify -d '{"jwt":"JWT here"}' -H 'Content-Type: application/json'`,
+    response: {
+      message: 'valid',
+      payload: {
+        vendor: { id: 'student-netid', name: 'Student Name', created: '2024-06-01T00:00:00Z', validUntil: '2025-12-31T23:59:59Z' },
+        diner: { name: 'joe' },
+        order: { pizzas: ['pep', 'cheese'] },
+      },
+    },
   },
   {
     method: 'GET',
@@ -26,6 +37,16 @@ orderRouter.endpoints = [
     requiresAuth: false,
     description: 'Get the JSON Web Key Set (JWKS) for independent JWT verification',
     example: `curl -X POST localhost:3000/.well-known/jwks.json`,
+    response: {
+      keys: [
+        {
+          kty: 'RSA',
+          kid: 'KID here',
+          n: 'Key value here',
+          e: 'AQAB',
+        },
+      ],
+    },
   },
 ];
 
