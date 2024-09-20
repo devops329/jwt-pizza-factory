7de9b465f89e64ebbfa512cb2029861b31030d81
Stephen Amos <amos.stephen.m@gmail.com>
2024-09-20 13:32:33 -0600

    Docs: add endpoint docs to logrouter

--- src/routes/logRouter.js
+++ src/routes/logRouter.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import DB from '../database/database.js';
 
 const logRouter = express.Router();
 
@@ -6,12 +7,20 @@ let logRes = 'Log Received';
 
 logRouter.endpoints = [
   {
-    method: 'GET',
+    method: 'POST',
     path: '/api/log',
     requiresAuth: false,
     description: 'Report a problem',
-    example: `curl -X POST $host/api/log -H 'authorization: Bearer abcxyz' -H 'Content-Type:application/json' -d '{"message": "Log Response"}'`,
+    example: `curl -X POST $host/api/log -H 'Content-Type:application/json' -d '{"message": "Log Response"}'`,
     response: 'Message',
+  },
+  {
+    method: 'PUT',
+    path: '/api/log/update',
+    requiresAuth: true,
+    description: 'Update the log response',
+    example: `curl -X PUT $host/api/log -H 'authorization: Bearer abcxyz' -H 'Content-Type:application/json' -d '{"message": "Updated Log Response"}'`,
+    response: 'Updated Log Response',
   },
 ];
 
@@ -27,6 +36,7 @@ const getAuthorizationInfo = async (req, res, next) => {
 logRouter.put('/update', getAuthorizationInfo, (req, res) => {
   const resMessage = req.body.message;
   logRes = resMessage;
+  res.send(logRes);
 });
 
 logRouter.post('/', (req, res) => {
