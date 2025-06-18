15a6a09e2dd1ac7a37c550d8007aa83921026bdf
Lee <leesjensen@gmail.com>
2025-06-18 16:30:47 -0600

    refactor(tests) to make it easier to mock endpoints conditionally

--- frontend/tests/dashboard.spec.ts
+++ frontend/tests/dashboard.spec.ts
@@ -2,18 +2,59 @@
 import { test, expect } from 'playwright-test-coverage';
 import { Vendor } from '../src/model';
 
+function getUrlParam(route, regex) {
+  const url = route.request().url();
+  const match = url.match(regex);
+  return match ? match[1] : null;
+}
+
 async function registerLoginHandlers(page, vendor) {
-  await page.route('**/api/vendor/test3', async (route) => {
-    await route.fulfill({
-      status: 200,
-      contentType: 'application/json',
-      body: JSON.stringify({ exists: true }),
-    });
+  await page.route('**/api/vendor/*', async (route, request) => {
+    const param = getUrlParam(route, /\/api\/vendor\/([^/]+)/);
+    if (param === 'code') {
+      // Send auth code email
+      console.log(`Intercepted API call: [${route.request().method()}] ${route.request().url()}`);
+      await route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify({ email: 'test3.byu.edu' }),
+      });
+    } else if (param === 'auth') {
+      // Authorize code received from email
+      console.log(`Intercepted API call: [${route.request().method()}] ${route.request().url()}`);
+      const postData = request.postData();
+      const json = postData ? JSON.parse(postData) : {};
+      expect(json).toEqual({ id: 'test3', code: '1234' });
+
+      await route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify(vendor),
+      });
+    } else {
+      // Check if vendor exists
+      console.log(`Intercepted API call: [${route.request().method()}] ${route.request().url()} (netId: ${param})`);
+      await route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify({ exists: vendor.id === param }),
+      });
+    }
   });
 
   await page.route('**/api/vendor', async (route) => {
+    console.log(`Intercepted API call: [${route.request().method()}] ${route.request().url()}`);
     const authHeader = route.request().headers()['authorization'];
     if (authHeader) {
+      // Update vendor
+      if (route.request().method() === 'PUT') {
+        const postData = route.request().postData();
+        if (postData) {
+          const updatedFields = JSON.parse(postData);
+          Object.assign(vendor, updatedFields);
+        }
+      }
+
       await route.fulfill({
         status: 200,
         contentType: 'application/json',
@@ -27,26 +68,14 @@ async function registerLoginHandlers(page, vendor) {
       body: JSON.stringify({ message: 'Not authorized' }),
     });
   });
+}
 
-  await page.route('**/api/vendor/code', async (route) => {
-    await route.fulfill({
-      status: 200,
-      contentType: 'application/json',
-      body: JSON.stringify({ email: 'test3.byu.edu' }),
-    });
-  });
-
-  await page.route('**/api/vendor/auth', async (route, request) => {
-    const postData = request.postData();
-    const json = postData ? JSON.parse(postData) : {};
-    expect(json).toEqual({ id: 'test3', code: '1234' });
-
-    await route.fulfill({
-      status: 200,
-      contentType: 'application/json',
-      body: JSON.stringify(vendor),
-    });
-  });
+async function login(page) {
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('textbox', { name: 'Login' }).fill('test3');
+  await page.getByRole('button', { name: 'Get code' }).click();
+  await page.getByRole('textbox', { name: 'Authenticate code' }).fill('1234');
+  await page.getByRole('button', { name: 'Validate code' }).click();
 }
 
 test('Login', async ({ page }) => {
@@ -76,16 +105,8 @@ test('Login', async ({ page }) => {
   await expect(page.getByText('Login')).toBeVisible();
 });
 
-async function login(page) {
-  await page.goto('http://localhost:5173/');
-  await page.getByRole('textbox', { name: 'Login' }).fill('test3');
-  await page.getByRole('button', { name: 'Get code' }).click();
-  await page.getByRole('textbox', { name: 'Authenticate code' }).fill('1234');
-  await page.getByRole('button', { name: 'Validate code' }).click();
-}
-
 test('Badge', async ({ page }) => {
-  const vendor = { id: 'test3', name: 'Test 3', apiKey: 'xyz', website: 'https://pizza.test.com' };
+  const vendor: Vendor = { id: 'test3', name: 'Test 3', apiKey: 'xyz', website: 'https://pizza.test.com' };
 
   await page.route('**/api/badge/test3/a?label=Example&value=100%25&color=%2344aa44', async (route) => {
     if (route.request().method() === 'POST') {
@@ -119,7 +140,7 @@ test('Badge', async ({ page }) => {
 });
 
 test('Chaos', async ({ page }) => {
-  const vendor: Vendor = { id: 'test3', name: 'Test 3', phone: '333-333-333', email: 'test3@byu.edu', apiKey: 'xyz', website: 'https://pizza.test.com', gitHubUrl: 'https://github.com/test3' };
+  const vendor: Vendor = { id: 'test3', name: 'Test 3', phone: '333-333-3333', email: 'test3@byu.edu', apiKey: 'xyz', website: 'https://pizza.test.com', gitHubUrl: 'https://github.com/test3' };
 
   await page.route('**/api/vendor/chaos/fail', async (route) => {
     if (route.request().method() === 'PUT') {
@@ -150,4 +171,28 @@ test('Chaos', async ({ page }) => {
   };
   await page.reload();
   await expect(page.locator('#chaosStatus')).toContainText('calm');
+});
+
+test('Update', async ({ page }) => {
+  const vendor: Vendor = { id: 'test3', name: 'Test 3', phone: '333-333-3333', email: 'test3@byu.edu', apiKey: 'xyz', website: 'https://pizza.test.com', gitHubUrl: 'https://github.com/test3' };
+
+  await registerLoginHandlers(page, vendor);
+  await login(page);
+  await page.getByRole('textbox', { name: 'Name:', exact: true }).fill('Test 4');
+  await page.getByRole('textbox', { name: 'Phone:' }).fill('333-333-3334');
+  await page.getByRole('textbox', { name: 'Email:' }).fill('test4@byu.edu');
+  await page.getByRole('textbox', { name: 'Pizza Service:' }).fill('https://pizza.test4.com');
+  await page.getByRole('textbox', { name: 'GitHub URL:' }).fill('https://github.com/test4');
+  await page.getByText('Name:Phone:Email:Pizza Service:GitHub URL:Update').click();
+  await page.getByRole('button', { name: 'Update' }).click();
+
+  await expect(page.getByRole('textbox', { name: 'Name:', exact: true })).toHaveValue('Test 4');
+  await expect(page.getByRole('textbox', { name: 'Phone:' })).toHaveValue('333-333-3334');
+  await expect(page.getByRole('textbox', { name: 'Email:' })).toHaveValue('test4@byu.edu');
+  await expect(page.getByRole('textbox', { name: 'Pizza Service:' })).toHaveValue('https://pizza.test4.com');
+  await expect(page.getByRole('textbox', { name: 'GitHub URL:' })).toHaveValue('https://github.com/test4');
+
+  await page.reload();
+
+  await expect(page.getByRole('textbox', { name: 'Name:', exact: true })).toHaveValue('Test 4');
 });
