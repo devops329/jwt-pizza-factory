0cb4644f40217f420ff4b9851b58d90d0c88dff5
Lee <leesjensen@gmail.com>
2025-06-18 17:38:43 -0600

    change(backend) to use port 4000 by default since service uses 3000 by default

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const app = require('./service');
-const port = process.argv[2] || 3000;
+const port = process.argv[2] || 4000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
